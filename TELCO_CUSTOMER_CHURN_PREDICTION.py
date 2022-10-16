fd34db5d2645b3e8a48bf7c3f6060d13a46c5bd9
Alp Furkan Kılıç <alpfurkankilic2@gmail.com>
2022-10-16 13:32:19 +0300

    Create TELCO_CUSTOMER_CHURN_PREDICTION.py
    It is expected to develop a machine learning model that can predict customers who will leave the company.

---
+++ TELCO_CUSTOMER_CHURN_PREDICTION.py
@@ -0,0 +1,471 @@
+import joblib
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier, AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_validate, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+import warnings
+warnings.simplefilter(action="ignore")
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+df = pd.read_csv("datasets/Telco-Customer-Churn.csv")
+df.head()
+
+df.isnull().sum()
+df.shape
+df.info()
+
+df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors='coerce')
+df.head()
+
+df["Churn"] = df["Churn"].apply(lambda x: 1 if x == "Yes" else 0)
+
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+
+check_df(df)
+
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if
+                   dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if
+                   dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    return cat_cols, num_cols, cat_but_car
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+cat_cols
+num_cols
+cat_but_car
+
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+
+for col in cat_cols:
+    cat_summary(df, col)
+
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+
+for col in num_cols:
+    target_summary_with_num(df, "Churn", col)
+
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(categorical_col)
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean(),
+                        "Count": dataframe[categorical_col].value_counts(),
+                        "Ratio": 100 * dataframe[categorical_col].value_counts() / len(dataframe)}), end="\n\n\n")
+
+
+for col in cat_cols:
+    target_summary_with_cat(df, "Churn", col)
+
+def correlation_matrix(df, cols):
+    fig = plt.gcf()
+    fig.set_size_inches(10, 8)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
+    fig = sns.heatmap(df[cols].corr(), annot=True, linewidths=0.5, annot_kws={'size': 12}, linecolor='w', cmap='RdBu')
+    plt.show(block=True)
+
+correlation_matrix(df, num_cols)
+
+df.isnull().sum()
+
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+    if na_name:
+        return na_columns
+
+
+na_columns = missing_values_table(df, na_name=True)
+
+df["TotalCharges"].fillna(df["TotalCharges"].median(), inplace=True)
+
+df.isnull().sum()
+
+##################################
+# Outlier analysis
+##################################
+
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def replace_with_thresholds(dataframe, variable, q1=0.05, q3=0.95):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable, q1=0.05, q3=0.95)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+# Aykırı Değer Analizi ve Baskılama İşlemi
+for col in num_cols:
+    print(col, check_outlier(df, col))
+    if check_outlier(df, col):
+        replace_with_thresholds(df, col)
+
+
+##################################
+# Feature Extraction
+##################################
+
+df.loc[(df["tenure"]>=0) & (df["tenure"]<=12),"TENURE_CLASS"] = "0-1 Year"
+df.loc[(df["tenure"]>12) & (df["tenure"]<=24),"TENURE_CLASS"] = "1-2 Year"
+df.loc[(df["tenure"]>24) & (df["tenure"]<=36),"TENURE_CLASS"] = "2-3 Year"
+df.loc[(df["tenure"]>36) & (df["tenure"]<=48),"TENURE_CLASS"] = "3-4 Year"
+df.loc[(df["tenure"]>48) & (df["tenure"]<=60),"TENURE_CLASS"] = "4-5 Year"
+df.loc[(df["tenure"]>60) & (df["tenure"]<=72),"TENURE_CLASS"] = "5-6 Year"
+
+df.head()
+
+df["NEW_Engaged"] = df["Contract"].apply(lambda x: 1 if x in ["One year","Two year"] else 0)
+
+df["NEW_noProt"] = df.apply(lambda x: 1 if (x["OnlineBackup"] != "Yes") or (x["DeviceProtection"] != "Yes") or (x["TechSupport"] != "Yes") else 0, axis=1)
+
+df["NEW_Young_Not_Engaged"] = df.apply(lambda x: 1 if (x["NEW_Engaged"] == 0) and (x["SeniorCitizen"] == 0) else 0, axis=1)
+
+
+# Kişinin toplam aldığı servis sayısı
+df['NEW_TotalServices'] = (df[['PhoneService', 'InternetService', 'OnlineSecurity',
+                                       'OnlineBackup', 'DeviceProtection', 'TechSupport',
+                                       'StreamingTV', 'StreamingMovies']]== 'Yes').sum(axis=1)
+
+df.head()
+
+df["NEW_FLAG_ANY_STREAMING"] = df.apply(lambda x: 1 if (x["StreamingTV"] == "Yes") or (x["StreamingMovies"] == "Yes") else 0, axis=1)
+
+df["NEW_FLAG_AutoPayment"] = df["PaymentMethod"].apply(lambda x: 1 if x in ["Bank transfer (automatic)","Credit card (automatic)"] else 0)
+
+df["NEW_AVG_Charges"] = df["TotalCharges"] / (df["tenure"] + 1)
+
+df["NEW_Increase"] = df["NEW_AVG_Charges"] / df["MonthlyCharges"]
+
+df["NEW_AVG_Service_Fee"] = df["MonthlyCharges"] / (df['NEW_TotalServices'] + 1)
+
+cat_cols = [col for col in cat_cols if col not in ["Churn"]]
+
+##################################
+# ENCODING
+##################################
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+# LABEL ENCODING
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+binary_cols = [col for col in df.columns if df[col].dtypes == "O" and df[col].nunique() == 2]
+binary_cols
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+
+
+df.head()
+# One-Hot Encoding
+cat_cols = [col for col in cat_cols if col not in binary_cols and col not in ["Churn", "NEW_TotalServices"]]
+cat_cols
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+df = one_hot_encoder(df, cat_cols, drop_first=True)
+
+df.head()
+df.shape
+
+######################################################
+# 3. Base Models
+######################################################
+
+y = df["Churn"]
+X = df.drop(["Churn","customerID"], axis=1)
+
+def base_models(X, y, scoring="roc_auc"):
+    print("Base Models....")
+    classifiers = [('LR', LogisticRegression()),
+                   ('KNN', KNeighborsClassifier()),
+                   ("SVC", SVC()),
+                   ("CART", DecisionTreeClassifier()),
+                   ("RF", RandomForestClassifier()),
+                   ('Adaboost', AdaBoostClassifier()),
+                   ('GBM', GradientBoostingClassifier()),
+                   ('XGBoost', XGBClassifier(use_label_encoder=False, eval_metric='logloss')),
+                   ('LightGBM', LGBMClassifier())
+                   ]
+
+    for name, classifier in classifiers:
+        cv_results = cross_validate(classifier, X, y, cv=3, scoring=scoring)
+        print(f"{scoring}: {round(cv_results['test_score'].mean(), 4)} ({name}) ")
+
+base_models(X, y, scoring="accuracy")
+
+'''
+Base Models....
+accuracy: 0.8043 (LR) 
+accuracy: 0.7657 (KNN) 
+accuracy: 0.7346 (SVC) 
+accuracy: 0.7307 (CART) 
+accuracy: 0.7917 (RF) 
+accuracy: 0.8001 (Adaboost) 
+accuracy: 0.7992 (GBM) 
+accuracy: 0.7819 (XGBoost) 
+accuracy: 0.7919 (LightGBM) 
+
+'''
+
+
+######################################################
+# 4. Automated Hyperparameter Optimization
+######################################################
+
+knn_params = {"n_neighbors": range(2, 50)}
+
+cart_params = {'max_depth': range(1, 20),
+               "min_samples_split": range(2, 30)}
+
+rf_params = {"max_depth": [8, 15, None],
+             "max_features": [5, 7, "auto"],
+             "min_samples_split": [15, 20],
+             "n_estimators": [200, 300]}
+
+xgboost_params = {"learning_rate": [0.1, 0.01],
+                  "max_depth": [5, 8],
+                  "n_estimators": [100, 200]}
+
+lightgbm_params = {"learning_rate": [0.01, 0.1],
+                   "n_estimators": [300, 500]}
+
+
+classifiers = [('KNN', KNeighborsClassifier(), knn_params),
+               ("CART", DecisionTreeClassifier(), cart_params),
+               ("RF", RandomForestClassifier(), rf_params),
+               ('XGBoost', XGBClassifier(use_label_encoder=False, eval_metric='logloss'), xgboost_params),
+               ('LightGBM', LGBMClassifier(), lightgbm_params)]
+
+
+
+def hyperparameter_optimization(X, y, cv=3, scoring="roc_auc"):
+    print("Hyperparameter Optimization....")
+    best_models = {}
+    for name, classifier, params in classifiers:
+        print(f"########## {name} ##########")
+        cv_results = cross_validate(classifier, X, y, cv=cv, scoring=scoring)
+        print(f"{scoring} (Before): {round(cv_results['test_score'].mean(), 4)}")
+
+        gs_best = GridSearchCV(classifier, params, cv=cv, n_jobs=-1, verbose=False).fit(X, y)
+        final_model = classifier.set_params(**gs_best.best_params_)
+
+        cv_results = cross_validate(final_model, X, y, cv=cv, scoring=scoring)
+        print(f"{scoring} (After): {round(cv_results['test_score'].mean(), 4)}")
+        print(f"{name} best params: {gs_best.best_params_}", end="\n\n")
+        best_models[name] = final_model
+    return best_models
+
+best_models = hyperparameter_optimization(X, y)
+
+'''
+Hyperparameter Optimization....
+########## KNN ##########
+roc_auc (Before): 0.7527
+roc_auc (After): 0.7979
+KNN best params: {'n_neighbors': 17}
+########## CART ##########
+roc_auc (Before): 0.6519
+roc_auc (After): 0.8243
+CART best params: {'max_depth': 5, 'min_samples_split': 7}
+########## RF ##########
+roc_auc (Before): 0.8238
+roc_auc (After): 0.8446
+RF best params: {'max_depth': 8, 'max_features': 7, 'min_samples_split': 20, 'n_estimators': 200}
+########## XGBoost ##########
+roc_auc (Before): 0.82
+roc_auc (After): 0.8412
+XGBoost best params: {'learning_rate': 0.01, 'max_depth': 5, 'n_estimators': 200}
+########## LightGBM ##########
+roc_auc (Before): 0.8315
+roc_auc (After): 0.843
+LightGBM best params: {'learning_rate': 0.01, 'n_estimators': 300}
+'''
+
+######################################################
+# 5. Stacking & Ensemble Learning (Birden fazla modeli bir arada kullanmak)
+######################################################
+
+def voting_classifier(best_models, X, y):
+    print("Voting Classifier...")
+
+    voting_clf = VotingClassifier(estimators=[('LightGBM', best_models["LightGBM"]),
+                                              ('RF', best_models["RF"]),
+                                              ('XGBoost', best_models["XGBoost"])],
+                                  voting='soft').fit(X, y)
+
+    cv_results = cross_validate(voting_clf, X, y, cv=3, scoring=["accuracy", "f1", "roc_auc"])
+    print(f"Accuracy: {cv_results['test_accuracy'].mean()}")
+    print(f"F1Score: {cv_results['test_f1'].mean()}")
+    print(f"ROC_AUC: {cv_results['test_roc_auc'].mean()}")
+    return voting_clf
+
+voting_clf = voting_classifier(best_models, X, y)
+
+######################################################
+# 6. Prediction for a New Observation
+######################################################
+
+X.columns
+random_user = X.sample(1, random_state=45)
+voting_clf.predict(random_user)
+
+'''
+Voting Classifier...
+Accuracy: 0.8034917532186147
+F1Score: 0.5820929607734779
+ROC_AUC: 0.8450557016545103
+Out[5]: array([0], dtype=int64)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################################
+# BASE MODEL
+##################################
+
+dff = df.copy()
+cat_cols = [col for col in cat_cols if col not in ["Churn"]]
+cat_cols
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+dff = one_hot_encoder(dff, cat_cols, drop_first=True)
+
+
+
+
+
+df.head()
+df.shape
+
+
+
+
+
+'''
+Voting Classifier...
+Accuracy: 0.8024978786939578
+F1Score: 0.5785977190313701
+ROC_AUC: 0.8447705261384892
+Out[5]: array([0], dtype=int64)
+'''
+
+joblib.dump(voting_clf, "voting_clf2.pkl") # modeli kaydediyoruz
+
+new_model = joblib.load("voting_clf2.pkl")
+new_model.predict(random_user)
